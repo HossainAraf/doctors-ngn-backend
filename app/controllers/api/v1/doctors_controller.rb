d9a750c3c42e8d54ce5127cd71486241844d71cb
Hossain <arafat.kd99@gmail.com>
2024-09-08 22:21:31 +0100

    Update Doctors controller to perform CRUD operation

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::DoctorsController < ApplicationController
+  def index
+    doctors= Doctor.all
+    render json: doctors
+  end
+
+  def show
+    doctor = Doctor.find(params[:id])
+    render json: doctor
+  end
+
+  def create
+    doctors = Doctor.new(doctor_params)
+    if doctor.save
+      render json: doctor, status: :created
+    else
+      render json: doctor.errors, status: :unprocessable_entity
+    end
+
+    private
+
+    def doctor_params
+      params.require(:doctor).permit(:name, :specification_id)
+    end
+  
 end
