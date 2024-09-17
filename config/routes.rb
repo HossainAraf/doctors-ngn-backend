3cbb860ed1c2832d9525588eae35709e21b7f27c
Hossain <arafat.kd99@gmail.com>
2024-09-17 04:17:54 +0100

    Limit the doctors route to only for specific actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :doctors
+      resources :doctors, only: [:index, :show, :create, :destroy, :update] 
       resources :specifications do
         get 'doctors', to: 'doctors#by_specification'
       end
