0d6ae83aa92f97cde9cabc2daa003812358c6705
Hossain <arafat.kd99@gmail.com>
2024-09-28 05:50:11 +0100

    Generate CreateDoctor migration

---
+++ db/migrate/20240928044626_create_doctors.rb
@@ -0,0 +1,18 @@
+class CreateDoctors < ActiveRecord::Migration[7.1]
+  def change
+    create_table :doctors do |t|
+      t.string :name, limit: 255, null: false
+      t.integer :specification_id, null: false
+      t.integer :display_order, null: false
+      t.string :degree, limit: 255
+      t.string :designation, limit: 255
+      t.string :chember, limit: 255
+      t.string :time, limit: 255
+      t.string :contact, limit: 255
+
+      t.timestamps
+    end
+
+    add_foreign_key :doctors, :specifications
+  end
+end
