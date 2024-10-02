d2897ddf162bef38b12fec5753f65c0195315315
Hossain <arafat.kd99@gmail.com>
2024-10-02 12:42:47 +0100

    Configure files for deployment

--- config/puma.rb
+++ config/puma.rb
@@ -16,6 +16,7 @@ if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+  workers ENV.fetch("WEB_CONCURRENCY") {4}
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
@@ -33,3 +34,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+preload_app!
