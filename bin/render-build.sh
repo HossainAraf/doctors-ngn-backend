22fa04275875841b8735e93bc8294996eb53d8cd
Hossain <arafat.kd99@gmail.com>
2024-10-05 14:50:14 +0100

    Remove asset:precompile & clean from render-build since it's api only app

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
