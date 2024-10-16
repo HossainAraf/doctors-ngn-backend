7cd69870616200ff573574eda5071b3e9546cb4c
Hossain <arafat.kd99@gmail.com>
2024-10-16 05:01:22 +0100

    Update 'update' method to update value of an entry as per id

--- app/controllers/api/v1/user_feedbacks_controller.rb
+++ app/controllers/api/v1/user_feedbacks_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserFeedbacksController < ApplicationController
   def index
-    user_feedbacks= UserFeedback.all
+    user_feedbacks = UserFeedback.all
     render json: user_feedbacks
   end
 
@@ -14,22 +14,32 @@ class Api::V1::UserFeedbacksController < ApplicationController
   end
 
   def update
-  end
-
-  def destroy
     user_feedback = UserFeedback.find_by(id: params[:id])
     if user_feedback
-      user_feedback.destroy
-      head :no_content  # This responds with 204 
+      if user_feedback.update(user_feedback_params)
+        render json: user_feedback, status: :ok  # Respond with the updated feedback data
+      else
+        render json: { error: 'Failed to update feedback' }, status: :unprocessable_entity
+      end
     else
       render json: { error: 'User feedback not found' }, status: :not_found
     end
   end
   
 
+  def destroy
+    user_feedback = UserFeedback.find_by(id: params[:id])
+    if user_feedback
+      user_feedback.destroy
+      head :no_content # This responds with 204
+    else
+      render json: { error: 'User feedback not found' }, status: :not_found
+    end
+  end
 end
 
 private
+
 def user_feedback_params
   params.require(:user_feedback).permit(:name, :email, :feedback, :phone)
 end
--- test/controllers/api/v1/feedback_forms_controller_test.rb
+++ test/controllers/api/v1/feedback_forms_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::FeedbackFormsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_feedback_forms_index_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get api_v1_feedback_forms_create_url
     assert_response :success
   end
 
-  test "should get update" do
+  test 'should get update' do
     get api_v1_feedback_forms_update_url
     assert_response :success
   end
