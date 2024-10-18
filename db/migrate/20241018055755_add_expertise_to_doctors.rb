a7b5aa12979c2d0763bc7751b0e365c8e0a50887
Hossain <arafat.kd99@gmail.com>
2024-10-18 12:20:22 +0600

    Add new column 'expertise' to doctors table

--- db/migrate/20241018055755_add_expertise_to_doctors.rb
+++ db/migrate/20241018055755_add_expertise_to_doctors.rb
@@ -1,5 +1,5 @@
 class AddExpertiseToDoctors < ActiveRecord::Migration[7.1]
   def change
-    add_column :doctors, :Expertise, :string
+    add_column :doctors, :expertise, :string, limit: 255
   end
 end
