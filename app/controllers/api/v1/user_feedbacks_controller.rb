379935adf36be81e2ff227348f219381088eab6a
Hossain <arafat.kd99@gmail.com>
2024-10-18 12:08:04 +0600

    Add a column 'expertise' to doctors table

--- app/controllers/api/v1/user_feedbacks_controller.rb
+++ app/controllers/api/v1/user_feedbacks_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UserFeedbacksController < ApplicationController
   def index
-    user_feedbacks = UserFeedback.all
+    user_feedbacks = UserFeedback.order(created_at: :desc)
     render json: user_feedbacks
-  end
+  end  
 
   def create
     user_feedback = UserFeedback.new(user_feedback_params)
